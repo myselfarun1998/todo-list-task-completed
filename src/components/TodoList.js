9e9faca0894f9a54d48780952c326b0a2226f270
myselfarun1998 <arunprofessional123@gmail.com>
2022-01-23 12:34:27 +0530

    1 file modified

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -58,11 +58,9 @@ class TodoList extends Component {
 
     const { taskTitle, date, time, emoji } = this.state.currentTodo;
 
-    if (!taskTitle || !date || !time || !emoji) {
+    if (!taskTitle || !emoji) {
       return alert('Fill all the details to save');
     }
-
-    // const data = event.target,
     const newTodo = {
       taskTitle,
       date,
@@ -172,7 +170,7 @@ class TodoList extends Component {
               placeholder="Enter the Time"
               name="time"
               defaultValue={this.time}
-              // value={this.state.currentTodo.time}
+              //value={this.state.currentTodo.time}
               onChange={this.handleChangeValues}
               ref={this.formBasicTime}
             />
